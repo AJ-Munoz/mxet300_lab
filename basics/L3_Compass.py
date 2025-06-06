fdbe5d6c357e6c9dafaf6a4843ae831164c681aa
AJ-Munoz <aj.munozvazquez@gmail.com>
2025-06-06 16:39:08 -0500

    Fr 6

---
+++ basics/L3_Compass.py
@@ -0,0 +1,11 @@
+import L2_compass_heading as compass
+import L1_log as log
+import time
+
+try:
+   while True:
+      print("Hi!")
+      time.sleep(0.2)
+
+except:
+   print("\n\n Bye Bye ,,,=^..^=,,")
